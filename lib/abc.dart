4064415296431a80aa494dd097c0091b00ca17ed
Asus <iqramulhasan65@gmail.com>
2024-10-16 00:28:41 +0600

    e

---
+++ lib/abc.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+class Myclass3 extends StatefulWidget {
+  const Myclass3({super.key});
+
+  @override
+  State<Myclass3> createState() => _Myclass3State();
+}
+
+class _Myclass3State extends State<Myclass3> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sifat"),
+      ),
+    );
+  }
+}
